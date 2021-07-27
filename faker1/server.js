804d84de2090e86976d5a1266c111101a680bea8
Hoda Shobaki <shobkihoda@gmail.com>
2021-07-27 11:56:26 +0300

    updated 27/7

--- faker1/server.js
+++ faker1/server.js
@@ -9,13 +9,21 @@ app.use( express.urlencoded({ extended: true }) );
 app.get("/api", (req, res) => {
   res.send("Our express api server is now sending this over to the browser");
 });
+const calssmembers = [
+  { firstName: "Ahmad",  lastName: "Shobki"    },
+  { firstName: "Mohammad", lastName: "Shobki"   },
+  { firstName: "Laila",  lastName: "Shobki"    },
+  { firstName: "Dyala", lastName: "Shobki"     },
+  { firstName: "Adnan", lastName: "Shobki" }
+];
 
 class User{
   constructor(){
     // var randomName = faker.name.findName(); 
     // var randomEmail = faker.internet.email(); 
     // var randomCard = faker.helpers.createCard(); 
-    this._id = Math.floor(Math.random() * (10000 - 1) + 1);
+    // this.id=faker.random()*(100-1)+1;
+    this.id = Math.floor(Math.random() * (1000 - 1) + 1);
     this.firstName = faker.name.firstName();
     this.lastName = faker.name.lastName();
     this.phoneNumber = faker.phone.phoneNumber();
@@ -25,7 +33,7 @@ class User{
 }
 class Company {
   constructor(){
-    this._id = Math.floor(Math.random() * (10000 - 1) + 1);
+    this.id = Math.floor(Math.random() * (100 - 1) + 1);
     this.name = faker.company.companyName();
     this.address = {
       
@@ -37,17 +45,17 @@ class Company {
     };
   }
 }
-app.get("/api/users/new", (req, res) => {
+app.get("/api/:users/:new", (req, res) => {
   res.json( new User() );
 });
 
-app.get("api/companies/new",(req,res)=>{
+app.get("/api/:companies/:new/:new",(req,res)=>{
   res.json(new Company());
 });
  
 
 
-app.get("api/user/company",(req,res)=>
+app.get("/api/:user/:company/:new/:new",(req,res)=>
 {
   res.json({Company:new Company(),User:new User()});
 
@@ -56,7 +64,16 @@ app.get("api/user/company",(req,res)=>
 app.get("/api", (req, res) => {
   res.send("Will it work");
 });
-
+// class Members:
+ app.get("/api/allclass",(req,res)=>{
+  res.json(calssmembers);
+ }) ;
+ app.post("/api/addmember",(req,res)=>{
+   calssmembers.push(req.body);
+ })
+app.get("/api/classmember/:id",(req,res)=>{
+  res.json(calssmembers[req.params.id]);
+});
 
 const server = app.listen(8000, () =>
   console.log(`Server is locked and loaded on port ${server.address().port}!`)
